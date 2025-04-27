f3815e44960fa94c8e2133e3ab7a6a4557343680
Xiaoshennb666 <3401483887@qq.com>
2025-04-27 20:55:36 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_3YGZ39wIf', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5h1gvc46k0bsg3hfq6fmde4mgj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_tKJlUB5hL', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'bhou74nvhqhfqsvv6e4pcp80', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
